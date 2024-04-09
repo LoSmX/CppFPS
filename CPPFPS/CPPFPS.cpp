81556c88cd299125c522732725f7e690577d2a2a
LoSmX <39199599+LoSmX@users.noreply.github.com>
2024-04-09 23:38:32 +0200

    add more features

--- CPPFPS/CPPFPS.cpp
+++ CPPFPS/CPPFPS.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <chrono>
+#include <vector>
+
 #include <Windows.h>
 
 using namespace std;
@@ -53,7 +55,7 @@ int main()
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
-    map += L"#....######...#";
+    map += L"#....######....#";
     map += L"#..............#";
     map += L"#..............#";
     map += L"#..............#";
@@ -78,19 +80,31 @@ int main()
         // Controls
         // HAndle CCW Rotation
         if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
-            fPlayerA -= (0.5f) * fElapsedTime;
+            fPlayerA -= (1.0f) * fElapsedTime;
 
         if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
-            fPlayerA += (0.5f) * fElapsedTime;
+            fPlayerA += (1.0f) * fElapsedTime;
 
+        // Move forward with w
         if (GetAsyncKeyState((unsigned short)'W') & 0x8000){
             fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
             fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+            // Check if coliding with wall
+            if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+                fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+                fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+            }
         }
 
+        // Move backwards with s
         if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
             fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
             fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+            // Check if coliding with wall
+            if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+                fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+                fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+            }
         }
 
         // For each collum on screen
@@ -101,6 +115,7 @@ int main()
             // Calculating distance to wall for each column.
             float fDistanceToWall = 0;
             bool bHitWall = false;
+            bool bBoundary = false;
 
             float fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
             float fEyeY = cosf(fRayAngle);
@@ -108,6 +123,7 @@ int main()
             while (!bHitWall && fDistanceToWall < fDepth) {
                 fDistanceToWall += 0.1f;
 
+                // 
                 int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
                 int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
 
@@ -120,11 +136,24 @@ int main()
                     // Ray is inbounds check if ray hits the wall
                     if (map[nTestY * nMapWidth + nTestX] == '#') {
                         bHitWall = true;
+
+                        // Check where are the corneers of wall
+                        vector<pair<float, float>> p; // distance, dot
+
+                        for (int tx = 0; tx < 2; tx++) {
+                            for (int ty = 0; ty < 2; ty++) {
+                                float vy = (float)nTestY + ty - fPlayerY;
+                                float vx = (float)nTestY + tx - fPlayerX;
+                                float d = sqrt(vx * vx + vy * vy);
+                                float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
+                                p.push_back(make_pair(d, dot));
+                            }
+                        }
                     }
                 }
             }
             // Calculate distance to ceiling and floor
-            int nCeiling = (int)( (float)(nScreenHeight / 2.0f - nScreenHeight / ((float)fDistanceToWall);
+            int nCeiling = (int)( (float)(nScreenHeight / 2.0f - nScreenHeight / ((float)fDistanceToWall)));
             int nFloor = nScreenHeight - nCeiling;
 
             // Shader map
@@ -139,20 +168,24 @@ int main()
                 if (y < nCeiling) {
                     screen[y * nScreenWidth + x] = ' ';
                 }
-                else if(y > nCeiling && y <= nFloor)
+                else if(y >= nCeiling && y <= nFloor)
                 {
                     screen[y * nScreenWidth + x] = nShade;
                 }
                 else
                 {
-                    screen[y * nScreenWidth + x] = ' ';
+                    // Shader map
+                    float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+                    if (b < 0.25f)              nShade = '#';    // Very Close
+                    else if (b < 0.50f)         nShade = 'x';
+                    else if (b < 0.75f)         nShade = '-';
+                    else                        nShade = '.';
+                    screen[y * nScreenWidth + x] = nShade;
                 }
             }
-
-
         }
 
-        screen[nScreenSize - 1] = '\0';
+        // screen[nScreenSize - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, nScreenSize, { 0,0 }, &dwBytesWritten);
     }
 
